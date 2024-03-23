66e3f9cb2ea9f3119bd4202f65db582d236142a9
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2024-03-23 20:40:56 +0700

    feat: configure nix gc

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,14 @@
     '';
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 15d";
+  };
 
   services.openssh.enable = true;
 
