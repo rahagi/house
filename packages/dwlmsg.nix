b1f22862dc9a15445e72bb981638f2537402eff3
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2024-06-15 23:47:14 +0700

    feat(desktop.environment): update dwl, clickable yambar tag

---
+++ packages/dwlmsg.nix
@@ -0,0 +1,35 @@
+{
+  pkgs ? import <nixpkgs> {},
+  lib ? pkgs.lib,
+  fetchgit,
+}:
+pkgs.stdenv.mkDerivation {
+  pname = "dwlmsg";
+  version = "master";
+
+  src = fetchgit {
+    url = "https://codeberg.org/notchoc/dwlmsg";
+    rev = "edb673cb90a1236b53aebd9802523b7a5b4f7858";
+    sha256 = "sha256-ZDz+cO6bGZjvusfWMiMBkmFEp45+vHOFfhcBFK0calQ=";
+  };
+
+  buildInputs = [
+    pkgs.pkg-config
+    pkgs.wayland
+    pkgs.wayland-protocols
+    pkgs.wayland-scanner
+  ];
+
+  outputs = ["out"];
+
+  makeFlags = [
+    "PREFIX=$(out)"
+  ];
+
+  meta = with lib; {
+    homepage = "https://codeberg.org/notchoc/dwlmsg";
+    description = "dwlmsg - send ipc messages to dwl";
+    platforms = platforms.linux;
+    mainProgram = "dwlmsg";
+  };
+}
