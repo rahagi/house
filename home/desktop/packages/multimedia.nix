5176df0d982750402ce39303e1c3504bb3784881
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2024-06-16 00:29:21 +0700

    feat: muzika

--- home/desktop/packages/multimedia.nix
+++ home/desktop/packages/multimedia.nix
@@ -4,10 +4,20 @@
     recursive = true;
   };
 
+  xdg.configFile."mpd" = {
+    source = ../../../config/mpd;
+    recursive = true;
+  };
+
+  xdg.configFile."ncmpcpp" = {
+    source = ../../../config/ncmpcpp;
+    recursive = true;
+  };
+
   services.easyeffects = {
     enable = true;
     preset = "HD 58X";
   };
 
-  home.packages = with pkgs; [mpv musescore pavucontrol];
+  home.packages = with pkgs; [mpv musescore pavucontrol mpd ncmpcpp mpc-cli];
 }
