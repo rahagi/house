27063af0f0c549a90ee6341124e345e934eb8d7c
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2024-10-13 18:23:40 +0700

    chore: remove unused shit

--- config/wal/templates/somebar-color.hpp
+++
@@ -1,4 +0,0 @@
-#define COLOR(hex) Color((hex >> 16) & 0xFF, (hex >> 8) & 0xFF, hex & 0xFF)
-
-constexpr ColorScheme colorInactive = {{COLOR(0x{foreground.strip}), COLOR(0x{background.strip})}};
-constexpr ColorScheme colorActive = {{COLOR(0x{foreground.strip}), COLOR(0x{color2.strip})}};
