88ed5f788dc107e5c6f4533b60e75fa5f31faddf
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2025-11-07 17:14:29 +0700

    fix: readjust terminal font size
    Signed-off-by: Rahagi Ahnaf Saidani <rahagi@protonmail.com>

--- home/desktop/terminal/foot.nix
+++ home/desktop/terminal/foot.nix
@@ -3,13 +3,14 @@
     enable = true;
     settings = {
       main = {
-        font = "monospace:size=17";
+        font = "monospace:size=15";
         dpi-aware = "yes";
         pad = "16x16";
       };
       key-bindings = {
         show-urls-launch = "Control+Shift+u";
         unicode-input = "none";
+        search-start = "none";
       };
     };
   };
