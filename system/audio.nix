5375b2f34fff043018b325dae8e1a533df365cd7
Rahagi Ahnaf Saidani <rahagi@protonmail.com>
2025-12-19 23:05:32 +0700

    fix: fix audio crackling

--- system/audio.nix
+++ system/audio.nix
@@ -6,10 +6,34 @@
       support32Bit = true;
     };
     pulse.enable = true;
+    extraConfig.pipewire-pulse = {
+      "01-fix-crackling" = {
+        context.modules = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              pulse.min.req = "1024/48000";
+              pulse.default.req = "1024/48000";
+              pulse.max.req = "1024/48000";
+              pulse.min.quantum = "1024/48000";
+              pulse.max.quantum = "1024/48000";
+            };
+          }
+        ];
+        stream.properties = {
+          node.latency = "1024/48000";
+          resample.quality = 1;
+        };
+      };
+    };
     extraConfig.pipewire = {
-      "01-fix-cracklng" = {
-        "clock.force-rate" = 48000;
-        "clock.force-quantum" = 1024;
+      "01-fix-crackling" = {
+        "context.properties" = {
+          "default.clock.force-rate" = 48000;
+          "default.clock.quantum" = 1024;
+          "default.clock.min-quantum" = 1024;
+          "default.clock.max-quantum" = 1024;
+        };
       };
     };
   };
